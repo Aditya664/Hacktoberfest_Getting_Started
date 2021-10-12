f79606394ac2d9ec44a39a8b37cf07b13b158227
Aditya <Adityadeshmukh7350@gmail.com>
2021-10-12 08:53:22 -0700

    New

---
+++ Cpp/Print all Numbers between the given integers a & b.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n Enter the two nos. => ";
+    int a,b;
+    cin >> a >> b;
+
+    for( int num = a; num <= b; num++ )
+    {
+        int i;
+        for( i = 2; i < num; i++)
+        {
+            if(num%i == 0)
+            {
+                break;
+            }
+        }
+        if( i == num )
+        {
+            cout << num << endl;
+        }
+    }
+
+        return 0;
+}
