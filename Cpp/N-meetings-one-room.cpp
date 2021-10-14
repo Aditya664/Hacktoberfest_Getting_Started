b97ac629209e5e1068cccbe1bf77bb541d5391ef
Restyled.io <commits@restyled.io>
2021-10-14 03:50:35 +0000

    Restyled by astyle

--- Cpp/N-meetings-one-room.cpp
+++ Cpp/N-meetings-one-room.cpp
@@ -14,13 +14,13 @@ int maxMeetings(int start[], int end[], int n)
 {
     int count=1;
     vector<pair<int,int>>v;
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++) {
         v.push_back({end[i],start[i]});
     }
     sort(v.begin(),v.end());
     int temp=v[0].first;
-    for(int i=1;i<n;i++){
-        if(v[i].second>temp){
+    for(int i=1; i<n; i++) {
+        if(v[i].second>temp) {
             count++;
             temp=v[i].first;
         }
@@ -29,15 +29,15 @@ int maxMeetings(int start[], int end[], int n)
 
 }
 
-int main() 
+int main()
 {
     int n;
     cin >> n;
     int start[n], end[n];
-    for (int i = 0; i < n; i++) 
-    cin >> start[i];
-    for (int i = 0; i < n; i++) 
-    cin >> end[i];
+    for (int i = 0; i < n; i++)
+        cin >> start[i];
+    for (int i = 0; i < n; i++)
+        cin >> end[i];
     cout<<maxMeetings(start, end, n);
     return(0);
 }
