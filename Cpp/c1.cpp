c49a1fcf55b78a8ccdcb715458b6ad3a8f8f5366
Restyled.io <commits@restyled.io>
2021-10-17 17:45:41 +0000

    Restyled by astyle

--- Cpp/c1.cpp
+++ Cpp/c1.cpp
@@ -1,41 +1,41 @@
 
-   
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class SinglyLinkedListNode {
-    public:
-        int data;
-        SinglyLinkedListNode *next;
+public:
+    int data;
+    SinglyLinkedListNode *next;
 
-        SinglyLinkedListNode(int node_data) {
-            this->data = node_data;
-            this->next = nullptr;
-        }
+    SinglyLinkedListNode(int node_data) {
+        this->data = node_data;
+        this->next = nullptr;
+    }
 };
 
 class SinglyLinkedList {
-    public:
-        SinglyLinkedListNode *head;
-        SinglyLinkedListNode *tail;
+public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
 
-        SinglyLinkedList() {
-            this->head = nullptr;
-            this->tail = nullptr;
+    SinglyLinkedList() {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data) {
+        SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head) {
+            this->head = node;
+        } else {
+            this->tail->next = node;
         }
 
-        void insert_node(int node_data) {
-            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
-
-            if (!this->head) {
-                this->head = node;
-            } else {
-                this->tail->next = node;
-            }
-
-            this->tail = node;
-        }
+        this->tail = node;
+    }
 };
 
 void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
